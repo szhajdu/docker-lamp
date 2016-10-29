f7a438bc096e9f51cfdddd35ab998588576eab29
Hajdu Szabolcs <sabee0418@gmail.com>
2016-10-29 22:22:44 +0200

    Fixed db connection test.

--- web/test_connection.php
+++ web/test_connection.php
@@ -5,7 +5,7 @@ $password = 'password';
 
 try
 {
-	$connection = new PDO('mysql:host=mysql;port=3306;dbname=test', $username, $password);
+	$connection = new PDO('mysql:host=db;port=3306;dbname=test', $username, $password);
 	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	echo 'Connected successfully';
